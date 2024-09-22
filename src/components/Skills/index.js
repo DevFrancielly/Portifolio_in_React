736fee4ffcfadad7289599822c878fa263518d09
Francielly Silva <devfrancielly@gmail.com>
2024-09-22 15:22:06 -0300

    :lipstick: feat: CSS styling of skills

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -7,32 +7,32 @@ const Skills = () => {
             <div className='skills'>
                 <div className='card'>
                     <p className='card-p'>HTML 5</p>
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"/>
+                    <img src="https://icongr.am/devicon/html5-plain.svg?size=128&color=faf5e4"/>
                     <footer></footer>
                 </div>
                 <div className='card'>
                     <p className='card-p'>CSS 3</p>
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" />
+                    <img src="https://icongr.am/devicon/css3-plain.svg?size=128&color=faf5e4" />
                     <footer></footer>
                 </div>
                 <div className='card'>
                     <p className='card-p'>JavaScript</p>
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" />
+                    <img src="https://icongr.am/devicon/javascript-plain.svg?size=128&color=faf5e4" />
                     <footer></footer>
                 </div>
                 <div className='card'>
                     <p className='card-p'>React</p>
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
+                    <img src="https://icongr.am/simple/react.svg?size=128&color=faf5e4&colored=false" />
                     <footer></footer>
                 </div>
                 <div className='card'>
                     <p className='card-p'>WordPress</p>
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/wordpress/wordpress-original.svg" />
+                    <img src="https://icongr.am/devicon/wordpress-plain.svg?size=128&color=faf5e4" />
                     <footer></footer>
                 </div>
                 <div className='card'>
                     <p className='card-p'>PHP</p>
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/php/php-original.svg" />
+                    <img src="https://icongr.am/devicon/php-plain.svg?size=128&color=faf5e4" />
                     <footer></footer>
                 </div>
             </div>
