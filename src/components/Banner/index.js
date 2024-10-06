dbad791088f25182006face89f9e720bf80d7732
Francielly Silva <devfrancielly@gmail.com>
2024-10-06 16:35:43 -0300

    :lipstick:feat: change background font

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -6,8 +6,8 @@ const Banner = () => {
             <div className='banner-text'>
                 <p className='banner-p'>Francielly Silva</p>
                 <div>
-                    <h1>DEVELOPER FRONT</h1>
-                    <h1 className='destac'>PORTIFOLIO</h1>
+                    <h1 className='destac'>DEVELOPER FRONT</h1>
+                    <h1 className='destac-p'>PORTIFOLIO</h1>
                 </div>
             </div>
         </header>
